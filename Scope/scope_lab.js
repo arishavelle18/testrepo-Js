e9b478d5dfd0e28d1e39e4a5de9252d095d2c2f2
arishavelle18 <arishavelle18@yahoo.com>
2024-08-22 01:22:51 -0400

    test level of scope

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,26 +1,31 @@
-// // Global scope
-// var globalVar = "I'm a global variable";
-// let globalLet = "I'm also global, but scoped with let";
-// const globalConst = "I'm a global constant";
-// {
-// // Block scope
-// var blockVar = "I'm a block-scoped var";
-// let blockLet = "I'm a block-scoped let";
-// const blockConst = "I'm a block-scoped const";
-// }
-
-
-// console.log(blockVar);
-// console.log(blockLet);
-// console.log(blockConst);
-
-
-function show(){
-var functionVar = "I'm a block-scoped var";
-let functionLet = "I'm a block-scoped let";
-const functionConst = "I'm a block-scoped const";
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+{
+// Block scope
+var globalVar = "I'm a block variable";
+let globalLet = "I'm also block, but scoped with let";
+const globalConst = "I'm a block constant";
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
 }
-show();
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceErro
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+
+
+// function show(){
+// var functionVar = "I'm a block-scoped var";
+// let functionLet = "I'm a block-scoped let";
+// const functionConst = "I'm a block-scoped const";
+// }
+// show();
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceErro
